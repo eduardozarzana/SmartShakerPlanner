4d097d13d28741da6ddac8a77fb6e1037fa8a848
Eduardo Zarzana <eduardozarzana@gmail.com>
2025-07-03 09:05:29 -0300

    Corrigir dependências e configuração do Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   base: '/SmartShakerPlanner/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
   }
-});
+})
