441fea3a813852af94c392f5a71fdf9e1206a1fc
eduardozarzana <106182954+eduardozarzana@users.noreply.github.com>
2025-07-21 16:10:25 -0300

    Update Card.tsx

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -1,23 +1,21 @@
+import React, { ReactNode, HTMLAttributes } from 'react';
 
-import React, { ReactNode } from 'react';
-
-interface CardProps {
+interface CardProps extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
-  className?: string;
   title?: string;
   actions?: ReactNode;
 }
 
-const Card: React.FC<CardProps> = ({ children, className = '', title, actions }) => {
+const Card: React.FC<CardProps> = ({ children, className = '', title, actions, ...props }) => {
   return (
-    <div className={`bg-white shadow-lg rounded-lg overflow-hidden ${className}`}>
+    <div {...props} className={`bg-white shadow-lg rounded-lg overflow-hidden ${className}`}>
       {(title || actions) && (
         <div className="px-4 py-3 border-b border-green-200 sm:px-6 flex justify-between items-center">
           {title && <h3 className="text-lg leading-6 font-medium text-green-800">{title}</h3>}
           {actions && <div>{actions}</div>}
         </div>
       )}
-      <div className="p-4 sm:p-6 text-green-700"> {}
+      <div className="p-4 sm:p-6 text-green-700">
         {children}
       </div>
     </div>
