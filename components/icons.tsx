e1b6f6384471869ffd319498ce937ddf6e2b1bc3
eduardozarzana <106182954+eduardozarzana@users.noreply.github.com>
2025-07-21 10:30:55 -0300

    Update icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -100,6 +100,12 @@ export const ArrowPathIcon: React.FC<IconProps> = ({ className = "w-6 h-6" }) =>
   </svg>
 );
 
+export const CheckCircleIcon: React.FC<IconProps> = ({ className = "w-6 h-6" }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+  </svg>
+);
+
 export const XCircleIcon: React.FC<IconProps> = ({ className = "w-6 h-6" }) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
     <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
